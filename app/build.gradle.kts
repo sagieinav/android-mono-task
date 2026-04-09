56044679b8e02b0f9c7ff411137d665cb6d80d3e
sagieinav <sagi.einav@icloud.com>
2026-04-09 21:32:16 +0300

    Bumped app ver to v0.9.0-beta.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.sagi.monotask"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.9.0-beta.1"
+        versionCode = 3
+        versionName = "0.9.0-beta.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
